ad5fad23fc15bfad043c26b2bf2615ee01453f4e
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2014-11-04 23:25:37 -0800

    Updates to adaptive-grid code.

--- emigrate/Migrate.py
+++ emigrate/Migrate.py
@@ -23,13 +23,15 @@ class Migrate(object):
     molar_conductivity = None
     j = 0
     solution = []
+    full_solution = []
     solver_info = None
     pH = 7
     epsilon = 0.75
     Kag = 0.01
     pointwave = .5
     t = 0
-    adaptive_grid = False
+    adaptive_grid = True
+    calls = 0
 
     def __init__(self, system):
         """Initialize with a system from the constructor class."""
@@ -88,61 +90,64 @@ class Migrate(object):
         if self.adaptive_grid is True:
             self.E = self.E # * self.first_derivative(self.x)
 
-    def flux(self, concentrations):
+    def flux(self):
         """Calculate the flux of chemical species."""
-        self.set_E(concentrations)
+        self.set_E(self.concentrations)
         if self.adaptive_grid is True:
+            cD = self.tile_n(self.diffusivity) * self.concentrations
             diffusion = \
-                (self.second_derivative(np.tile(self.diffusivity,
-                                                (1, len(self.z)))
-                                       * concentrations) -
-                 self.first_derivative(np.tile(self.diffusivity,
-                                                (1, len(self.z)))
-                                        * concentrations) *
-                                        np.tile(self.second_derivative(self.x), (self.M, 1))/
-                                        np.tile(self.first_derivative(self.x), (self.M, 1)))/\
-                np.tile(self.first_derivative(self.x)**2, (self.M, 1))
+                (self.second_derivative(cD) -
+                 self.first_derivative(cD) * self.tile_m(self.xzz / self.xz)) / \
+                 self.tile_m(self.xz**2)
 
-            advection = np.tile(self.mobility, (1, len(self.z)))*concentrations
-            advection *= np.tile(self.first_derivative(self.E) - \
-                         self.second_derivative(self.x) / \
-                         self.first_derivative(self.x) * \
-                         self.E, (self.M, 1))
-            advection += self.first_derivative(np.tile(self.mobility, (1, len(self.z)))*concentrations) *\
-                         np.tile(self.E, (self.M, 1))
+            advection = (self.tile_n(self.mobility) * self.concentrations) *\
+                self.tile_m(self.first_derivative(self.E) -
+                    (self.xzz/self.x) * self.E) + self.first_derivative(
+                    (self.tile_n(self.mobility) * self.concentrations)) *\
+                    self.tile_m(self.E)
+            advection /= -self.tile_m(self.xz**2)
 
-            advection /= -self.first_derivative(self.x)**2
+
+            # advection = np.tile(self.mobility, (1, len(self.z)))*self.concentrations
+            # advection *= np.tile(self.first_derivative(self.E) - \
+            #              self.second_derivative(self.x) / \
+            #              self.first_derivative(self.x) * \
+            #              self.E, (self.M, 1))
+            # advection += self.first_derivative(np.tile(self.mobility, (1, len(self.z)))*self.concentrations) *\
+            #              np.tile(self.E, (self.M, 1))
+            #
+            # advection /= -self.first_derivative(self.x)**2
         else:
             diffusion = \
                 self.second_derivative(np.tile(self.diffusivity,
                                                (1, len(self.z)))
-                                       * concentrations
+                                       * self.concentrations
                                        )
             advection = \
                 -self.first_derivative(np.tile(self.mobility,
                                                (1, len(self.z)))
-                                       * concentrations *
+                                       * self.concentrations *
                                        self.E
                                        )
 
         total_flux = diffusion + advection
         return total_flux
 
-    def node_flux(self, x, concentrations):
+    def node_flux(self):
         """Calculate the flux of nodes."""
         if self.adaptive_grid is True:
             flux = self.pointwave *\
-                self.first_derivative(self.node_cost(concentrations) *
-                                      self.first_derivative(x))
-            flux = np.convolve(flux, gaussian(self.N, self.N*0.05), 'same')
+                self.first_derivative(self.node_cost() *
+                                      self.first_derivative(self.x))
+            flux = np.convolve(flux, gaussian(self.N, self.N*0.1), 'same')
             flux[0,] = flux[-1,] = 0
         else:
-            flux = np.zeros(x.shape)
+            flux = np.zeros(self.x.shape)
         return flux
 
-    def node_cost(self, concentrations):
+    def node_cost(self):
         """Calculate the cost function of each node."""
-        deriv = np.abs(self.first_derivative(concentrations))
+        deriv = np.abs(self.first_derivative(self.concentrations))
         cost = deriv / np.tile(np.nanmax(deriv, 1), (len(self.z), 1)).T
         cost = np.nanmax(cost, 0) + self.Kag
         return cost
@@ -161,19 +166,43 @@ class Migrate(object):
         return state
 
     def reshaped_flux(self, t, state):
+        """1-D flux function for ode solver."""
         if not t == self.t:
             self.calc_equilibrium()
             self.t = t
-        (x, concentrations) = self.decompose_state(state)
+        (self.x, self.concentrations) = self.decompose_state(state)
+        self.set_derivatives()
 
-        x_flux = self.node_flux(x, concentrations)
-        ion_flux = self.flux(concentrations)
+        x_flux = self.node_flux()
+        ion_flux = self.flux()
         flux = self.compose_state(x_flux, ion_flux)
         return flux
+
+    def set_derivatives(self):
+        self.xz = self.first_derivative(self.x)
+        self.xzz = self.second_derivative(self.x)
+        # self.phiz =
+        # self.phizz =
+
+    def tile_m(self, tileable):
+        return np.tile(tileable, (self.M, 1))
+
+    def tile_n(self, tileable):
+        return np.tile(tileable, (1, self.N))
+
+    def write_solution(self, t, state, full=True):
+        """Write the current state to solutions."""
+        (x, concentrations) = self.decompose_state(state)
+        if full is False:
+            self.solution.append([t, x, concentrations])
+        else:
+            self.full_solution.append([t, x, concentrations])
+        return None
 
     def solve(self, t, method='rk45'):
         """Solve for a series of time points using an ODE solver."""
         self.solution = []
+        self.full_solution = []
         solver = integrate.ode(self.reshaped_flux)
 
         if method == 'lsoda':
@@ -191,16 +220,15 @@ class Migrate(object):
         elif method == 'zvode':
             solver.set_integrator('zvode')
 
+        solver.set_solout(self.write_solution)
         solver.set_initial_value(self.compose_state(self.x, self.concentrations))
+
         for tp in t[1:-1]:
             solver.integrate(tp)
-            self.solution.append(self.decompose_state(solver.y))
+            self.write_solution(solver.t, solver.y, False)
             if not solver.successful():
                 print 'solver failed at time', tp
                 break
-
-        # self.solution = [sol.reshape(self.concentrations.shape)
-        #                  for sol in self.solution]
 
     def calc_equilibrium(self):
         pass
