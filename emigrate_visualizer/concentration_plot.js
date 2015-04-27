9d9dae3fc1c7eb77228b2ce2b15fea70cde0e9ac
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2015-04-26 20:39:21 -0700

    App updates.

--- emigrate_visualizer/concentration_plot.js
+++ emigrate_visualizer/concentration_plot.js
@@ -1,8 +1,5 @@
-// d3 = require('d3.min.js');
-
-data = d3.json('tutorial_example.json');
-
-var margin = {top: 30, right: 20, bottom: 30, left: 50},
+// set plot size
+var margin = {top: 30, right: 20, bottom: 30, left: 100},
 width = 600 - margin.left - margin.right,
 height = 270 - margin.top - margin.bottom;
 
@@ -12,57 +9,116 @@ var x = d3.scale.linear().range([0, width]);
 var y = d3.scale.linear().range([height, 0]);
 
 // Define the axes
-var xAxis = d3.svg.axis().scale(x)
-.orient("bottom").ticks(5);
+var xAxis = d3.svg.axis()
+                  .scale(x)
+                  .orient("bottom")
+                  .ticks(5);
 
-var yAxis = d3.svg.axis().scale(y)
-.orient("left").ticks(5);
+var yAxis = d3.svg.axis()
+                  .scale(y)
+                  .orient("left")
+                  .ticks(5);
 
 // Define the line
 var valueline = d3.svg.line()
-.x(function(d) { return x(d.date); })
-.y(function(d) { return y(d.close); });
+                      .x(function(d) { return x(d[0]); })
+                      .y(function(d) { return y(d[1]); });
 
 // Adds the svg canvas
 var svg = d3.select("body")
-.append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-.append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Get the data
-data = d3.json('tutorial_example.json', function(error, data){
-    console.log(data);
-    for(i = 0; i < data.electrolytes[0][1].concentrations.length; i++){
-        data.date = data.electrolytes[0][1].nodes;
-        data.close = data.electrolytes[0][1].concentrations[i];
-        data2=[]
-        for(i = 0; i < data.date.length; i++){
-            data2[i] = {date: data.date[i], close: data.close[i]}
-        }
+// Set up color options
+var p=d3.scale.category10();
 
+// Function for setting axis attributes
+var make_axes = function(data2){
+    // Scale the range of the data
+    x.domain([0, d3.max(data2, function(d) { return d[0]; })]);
+    y.domain([0, 2*d3.max(data2, function(d) { return d[1]; })]);
 
-        // Scale the range of the data
-        x.domain([0, d3.max(data2, function(d) { return d.date; })]);
-        y.domain([0, d3.max(data2, function(d) { return d.close; })]);
+    // Add the X Axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
+    // Add the Y Axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
 
-        // Add the valueline path.
-        svg.append("path")
-            .attr("class", "line")
-            .attr("d", valueline(data2))
+    //add xlabel
+    svg.append("text")
+       .attr("class", "x label")
+       .attr("text-anchor", "end")
+       .attr("x", width)
+       .attr("y", height - 6)
+       .text("length (m)");
 
-        // Add the X Axis
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
+    //add ylabel
+    svg.append("text")
+       .attr("class", "y label")
+       .attr("text-anchor", "end")
+       .attr("y", -50)
+       .attr("dy", ".75em")
+       .attr("transform", "rotate(-90)")
+       .text("concentration (M)");
+}
 
-        // Add the Y Axis
-        svg.append("g")
-            .attr("class", "y axis")
-            .call(yAxis);
+// Plot button info
+var frame = 0
+var lines = []
+var saved_data = null
+var plot = function(){
+    frame = 0
+    var file = document.getElementById("file_input")
+                       .value
+                       .replace("C:\\fakepath\\", "");
+
+    plotter = d3.json(file, function(error, data){
+        saved_data = data;
+        for(i = 0; i < data.electrolytes[frame][1].concentrations.length; i++){
+            if(i === 0){make_axes(d3.zip(
+                data.electrolytes[frame][1].nodes,
+                data.electrolytes[frame][1].concentrations[i]
+            ))};
+
+            // Add the valueline path.
+            lines[i] = svg.append("path")
+                            .attr("class", "line")
+                            .attr("d", valueline(d3.zip(
+                                data.electrolytes[frame][1].nodes,
+                                data.electrolytes[frame][1].concentrations[i]
+                            )))
+                            .attr("stroke", p(i%10))
 
         }});
+    };
+
+var next = function(time){
+    frame++;
+    update_lines(frame, time);
+};
+
+var prev = function(time){
+    frame--;
+    update_lines(frame, time);
+}
+
+var update_lines = function(frame, time){
+    for(i = 0; i < saved_data.electrolytes[frame][1].concentrations.length; i++){
+        lines[i].transition().duration(time)
+                .attr("d", valueline(d3.zip(
+                    saved_data.electrolytes[frame][1].nodes,
+                    saved_data.electrolytes[frame][1].concentrations[i])))}};
+
+var play = function(){
+    while(frame<saved_data.electrolytes.length){
+        next(1000)
+    }
+}
---
+++ package.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+asar pack emigrate_visualizer/ Emigrate.app/Contents/Resources/app.asar
