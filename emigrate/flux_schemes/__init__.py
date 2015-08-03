6dd0c0252400d437da8f95ad2701a749f6d8f148
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2015-08-02 19:53:41 -0700

    Removed references to old modules.

--- emigrate/flux_schemes/__init__.py
+++ emigrate/flux_schemes/__init__.py
@@ -1,14 +1,10 @@
 from Fluxer import Fluxer
 from Compact import Compact
-from CompactAdaptive import CompactAdaptive
 from SLIP import SLIP
-from minmod_limited import MinmodLimited
 
 fluxers = {None: Fluxer,
            'compact': Compact,
-           'adaptive_grid': CompactAdaptive,
            'slip': SLIP,
-           'minmod': MinmodLimited,
            }
 
 if __name__ == '__main__':
