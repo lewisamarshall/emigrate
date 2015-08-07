45480d7208a494b712d169dfa495d9cfa82d6dc8
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2015-08-06 23:32:43 -0700

    Fix FrameSeries for 3.x.

--- emigrate/FrameSeries.py
+++ emigrate/FrameSeries.py
@@ -1,6 +1,7 @@
 import h5py
 import json
 import numpy as np
+import sys
 from collections import OrderedDict
 from Frame import Frame
 
@@ -43,7 +44,10 @@ class FrameSeries(object):
                                                       (len(self.ions),),
                                                       dtype=string_dt)
                 for idx, value in enumerate(self.ions):
-                    self._ions[idx] = value
+                    if sys.version_info < (3,):
+                        self._ions[idx] = value
+                    else:
+                        self._ions[idx] = mystring.encode('ascii')
 
     def __getitem__(self, frame):
         frame = str(frame)
