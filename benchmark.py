096c6d663cddd41c6d3015f4d34d584f443dda2d
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2015-08-08 23:40:34 -0700

    Added benchmark.

---
+++ benchmark.py
@@ -0,0 +1,31 @@
+import emigrate
+import ionize
+import cProfile
+
+solutions = [ionize.Solution(['hepes', 'tris'],
+                             [.05, .105]),
+             ionize.Solution(['caproic acid', 'tris', 'fluorescein',
+                              'alexa fluor 488', 'mops', 'acetic acid'],
+                             [.01, .1, .01, .01, .01, .01]),
+             ionize.Solution(['hydrochloric acid', 'tris'],
+                             [.04, .08]),
+             ]
+
+system = emigrate.Frame(dict(
+                         n_nodes=137,
+                         lengths=[.005, .001, .02],
+                         interface_length=.0005,
+                         solutions=solutions,
+                         current=-500.,
+                         ))
+
+
+solver = emigrate.Solver(system,
+                         filename='examples/benchmark.hdf5',
+                         precondition=True,
+                         flux_mode='slip')
+tmax = 200
+dt = 1
+ode_solver = 'dopri5'
+profile = True
+cProfile.run("solver.solve(dt, tmax)")
