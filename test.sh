889978ed88513cb784837b1ab2f29eb2135890c2
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2015-08-16 13:18:09 -0700

    Change to figure out why echo fails py3.4

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ assert_raises 'emigrate construct -i examples/constructor.json -o examples/initi
 assert_raises 'emigrate load examples/initial_condition.json solve --output examples/cli_test.hdf5 -t 10.0 -d 1.0'
 assert_raises 'emigrate load examples/cli_test.hdf5 echo -f 5'
 assert_raises 'emigrate load examples/cli_test.hdf5 plot -f 5 examples/test_plot.png'
-emigrate load examples/cli_test.hdf5 plot -f 5 examples/test_plot.png
+emigrate load examples/cli_test.hdf5 echo -f 5
 assert_end
