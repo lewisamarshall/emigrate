8c037742eaabdb7c51811bec80695802914a808b
Lewis A. Marshall <lewis.a.marshall@gmail.com>
2015-08-16 12:47:14 -0700

    Added cli test.

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . assert.sh
-
+python -m emigrate.cli construct -i examples/constructor.json -o examples/initial_condition.json
 assert_raises 'python -m emigrate.cli construct -i examples/constructor.json -o examples/initial_condition.json'
 assert_raises 'python -m emigrate.cli load examples/initial_condition.json solve --output examples/cli_test.hdf5 -t 10.0 -d 1.0'
 assert_raises 'python -m emigrate.cli load examples/cli_test.hdf5 echo -f 5'
